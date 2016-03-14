a17ebe85aa5e368bfd730187df53fc2934819323
Thomas Bealer <tbealer1991@gmail.com>
2016-03-14 14:53:24 -0600

    migration made and 8 types seeded

---
+++ migrations/20160314134222_beertypes.js
@@ -0,0 +1,32 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('beertypes', function(table) {
+    table.increments();
+    table.string('colornum');
+    table.string('bitterness');
+    table.string('alcoholnum');
+    table.string('foodpairings');
+    table.string('glassware');
+    table.string('servingtemp');
+    table.string('colortxt');
+    table.string('clarity');
+    table.string('carbonationvisual');
+    table.string('alcoholtxt');
+    table.string('hop');
+    table.string('malt');
+    table.string('esters');
+    table.string('phenols');
+    table.string('body');
+    table.string('carbonationtaste');
+    table.string('finishlength');
+    table.string('attenuation');
+    table.string('hopsingr');
+    table.string('maltingr');
+    table.string('water');
+    table.string('yeast');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('beertypes');
+};
