00d9cc38c8ef6c2b2b6a0ee5b98d65ba3a4738af
Thomas Bealer <tbealer1991@gmail.com>
2016-03-15 14:33:06 -0600

    Wes just saved this project and my rep

--- src/server/routes/beers.js
+++ src/server/routes/beers.js
@@ -7,10 +7,48 @@ router.get('/beers', function (req, res, next) {
   
   knex.select('*').from('beertypes')
   .then(function (beers) {
-    console.log(beers);
-        res.render('beers', {beers: beers});
-      });
+      // our standard beers
+      var typesOfBeers = ["american", "bohemian", "belgian", "german", "english"];
+      
+      var sortedBeers = beers.reduce(function (accumulator, beer) {
+        var beerType = beer.name.replace("-", " ").split(" ")[0].toLowerCase();
 
+        // Check for the type of the beer, if it is one of our standard beers
+        if ( typesOfBeers.indexOf(beerType) >= 0 ) {
+          // if type is already inside the accumulator
+          if ( accumulator[beerType] ) {
+            // push into the already existing array
+            // e.g. accumulator = { "american": ["some beer"] };
+            //      accumulator["american"].push("some other beer")
+            //      accumulator => { "american": ["some beer", "some other beer"] };
+            accumulator[beerType].push(beer);
+          } else {
+            // if it's not in the accumulator, create a new array
+            // e.g. accumulator = { "american": ["some beer"] };
+            //      accumulator["belgian"] = ["some belgian beer"]
+            //      accumulator => { "american": ["some beer"], "belgian": ["some belgian beer"] };
+            accumulator[beerType] = [beer];
+          }
+        } else {
+          // If the type of the beer is not in the typesOfBeers array...
+          accumulator.others.push(beer);
+        }
+
+        // obj = { b: 5 };
+        // obj.a = 3
+        // { a: 3, b: 5 }
+        // typesOfBeers.forEach(function (style) {
+        //   console.log(accumulator, style, beer.name);
+        // });
+        return accumulator;
+      }, { others: [] });
+
+
+      console.log(sortedBeers);
+      res.render('beers', sortedBeers); 
+    });
 });
+
+     
 
 module.exports = router;
