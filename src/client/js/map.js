eb5aa084af70f380feea721d2b20aa219471727c
Tyler Rozboril <ty.rozboril@gmail.com>
2016-03-16 23:00:41 -0600

    pin and de-pin breweries on pubcrawl

--- src/client/js/map.js
+++ src/client/js/map.js
@@ -1,7 +1,7 @@
 var locations = [
 //     // ['Brewery Name', 'Address, City, State', 'Zip', 'description', 'image'],
     // ['Tylers Brewery', '10261 Macedonia St., Longmont, CO', '80503', 'Very nice and neat brewery'],
-];
+    ];
 
 // console.log(locations);
 // exports.up = function(knex, Promise) {
@@ -19,111 +19,83 @@ var locations = [
 //   });
 // };
 var styles = [{
-    featureType: 'all',
-    elementType: 'labels',
-    stylers: [
-      { visibility: 'on' }  
-    ]
-  }, {
-    featureType: 'water',
-    elementType: 'geometry',
-    stylers: [
-      { color: '#4a6eff',
-        visibility: 'on' }  
-    ]
-  }, {
-    featureType: 'landscape',
-    elementType: 'geometry',
-    stylers: [
-      { color: '#c7c7c7',
-        visibility: 'on' }  
-    ]
-  }, {
-    featureType: 'poi',
-    elementType: 'geometry',
-    stylers: [
-      { color: '#a6a6a6',
-        visibility: 'on' }  
-    ]
-  }, {
-    featureType: 'transit',
-    elementType: 'geometry', 
-    stylers: [
-      { color: '#a6a6a6',
-        visibility: 'on' }  
-    ]
-  }, {
-    featureType: 'road.highway',
-    elementType: 'geometry',
-    stylers: [
-      { color: '#404040',
-        visibility: 'on' }  
-    ]
-  }, {
-    featureType: 'road.arterial',
-    elementType: 'geometry',
-    stylers: [
-      { color: '#ecf0f1',
-        visibility: 'on' }  
-    ]
-  }];   
+  featureType: 'all',
+  elementType: 'labels',
+  stylers: [
+  { visibility: 'on' }  
+  ]
+}, {
+  featureType: 'water',
+  elementType: 'geometry',
+  stylers: [
+  { color: '#4a6eff',
+  visibility: 'on' }  
+  ]
+}, {
+  featureType: 'landscape',
+  elementType: 'geometry',
+  stylers: [
+  { color: '#c7c7c7',
+  visibility: 'on' }  
+  ]
+}, {
+  featureType: 'poi',
+  elementType: 'geometry',
+  stylers: [
+  { color: '#a6a6a6',
+  visibility: 'on' }  
+  ]
+}, {
+  featureType: 'transit',
+  elementType: 'geometry', 
+  stylers: [
+  { color: '#a6a6a6',
+  visibility: 'on' }  
+  ]
+}, {
+  featureType: 'road.highway',
+  elementType: 'geometry',
+  stylers: [
+  { color: '#404040',
+  visibility: 'on' }  
+  ]
+}, {
+  featureType: 'road.arterial',
+  elementType: 'geometry',
+  stylers: [
+  { color: '#ecf0f1',
+  visibility: 'on' }  
+  ]
+}];   
 var geocoder;
 var map;
 var bounds = new google.maps.LatLngBounds();
 function init() {
-    map = new google.maps.Map(
+  map = new google.maps.Map(
     document.getElementById("map_canvas"), {
-        center: new google.maps.LatLng(39.7392, -104.9903),
-        zoom: 13,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        styles: styles
+      center: new google.maps.LatLng(39.7392, -104.9903),
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles: styles
     });
-    geocoder = new google.maps.Geocoder();
-    for (i = 0; i < locations.length; i++) {
-        geocodeAddress(locations, i);
-    }
+  geocoder = new google.maps.Geocoder();
+  for (i = 0; i < locations.length; i++) {
+    geocodeAddress(locations, i);
+  }
 }
+
 google.maps.event.addDomListener(window, "load", init);
 function geocodeAddress(locations, i) {
-    var title = locations[i][0];
-    var address = locations[i][1];
-    var description = locations[i][3];
-    var page = locations[i][4];
-    geocoder.geocode({
-        'address': locations[i][1]
-    },
-    function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            var marker = new google.maps.Marker({
-                icon: 'http://maps.google.com/mapfiles/ms/icons/blue.png',
-                map: map,
-                position: results[0].geometry.location,
-                title: title,
-                animation: google.maps.Animation.DROP,
-                address: address,
-                description: description,
-                page: page
-            });
-            infoWindow(marker, map, title, address, description, page);
-            bounds.extend(marker.getPosition());
-            map.fitBounds(bounds);
-        } else {
-            alert("geocode of " + address + " failed:" + status);
-        }
-    });
-}
-function infoWindow(marker, map, title, address, description, page) {
-    google.maps.event.addListener(marker, 'click', function () {
-        var html = "<div><h3>" + title + "</h3><p>" + address + "<br><br>" + description + "<br></div><a href='" + page + "'>View Brewery Info</a></p></div>";
-        iw = new google.maps.InfoWindow({
-            content: html,
-            maxWidth: 350
-        });
-        iw.open(map, marker);
-    });
-}
-function createMarker(results) {
-    var marker = new google.maps.Marker({
+  var title = locations[i].name;
+  var address = locations[i].address;
+  var description = locations[i].description;
+  var page = locations[i][4];
+  geocoder.geocode({
+    'address': locations[i].address
+  },
+  function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var marker = new google.maps.Marker({
         icon: 'http://maps.google.com/mapfiles/ms/icons/blue.png',
         map: map,
         position: results[0].geometry.location,
@@ -131,31 +103,68 @@ function createMarker(results) {
         animation: google.maps.Animation.DROP,
         address: address,
         description: description,
-        image: image
+        page: page
+      });
+      infoWindow(marker, map, title, address, description, page);
+      bounds.extend(marker.getPosition());
+      map.fitBounds(bounds);
+    } else {
+      alert("geocode of " + address + " failed:" + status);
+    }
+  });
+}
+function infoWindow(marker, map, title, address, description, page) {
+  google.maps.event.addListener(marker, 'click', function () {
+    var html = "<div><h3>" + title + "</h3><p>" + address + "<br><br>" + description + "<br></div><a href='" + page + "'>View Brewery Info</a></p></div>";
+    iw = new google.maps.InfoWindow({
+      content: html,
+      maxWidth: 350
     });
-    bounds.extend(marker.getPosition());
-    map.fitBounds(bounds);
-    infoWindow(marker, map, title, address, url);
-    return marker;
+    iw.open(map, marker);
+  });
+}
+function createMarker(results) {
+  var marker = new google.maps.Marker({
+    icon: 'http://maps.google.com/mapfiles/ms/icons/blue.png',
+    map: map,
+    position: results[0].geometry.location,
+    title: title,
+    animation: google.maps.Animation.DROP,
+    address: address,
+    description: description,
+    image: image
+  });
+  bounds.extend(marker.getPosition());
+  map.fitBounds(bounds);
+  infoWindow(marker, map, title, address, url);
+  return marker;
 }
 
 $(document).ready(function () {
-    var numBreweries = $('.brewpub').length;
+  var numBreweries = $('.brewpub').length;
 
-    var breweryName;
-    var breweryAddress;
-    var breweryZip;
-    var breweryDescription;
-    var breweryLocation = [];
-
-    $('.brewery').on('click', function () {
-    var breweryName = $('p:eq(0)').text();
-    var breweryAddress = $('p:eq(1)').text();
-    var breweryZip = $('p:eq(2)').text();
-    var breweryDescription = $('p:eq(3)').text();
-    var breweryLocation = [breweryName, breweryAddress, breweryZip, breweryDescription];
-    locations.push(breweryLocation);
-    console.log(locations);
-    init();
-});
+  $('.brewery').each(function () {
+    $(this).on('click', function () {
+      var newObj = {
+        name: $(this).parent().children('.name').text(),
+        address: $(this).parent().children('.address').text(),
+        zip: $(this).parent().children('.zip').text(),
+        description: $(this).parent().children('.description').text()
+      };
+      locations.push(newObj);
+      init();
+    });
+  });
+  $('.removebrewery').each(function () {
+    $(this).on('click', function () {
+      var theName = $(this).parent().children('.name').text();
+      for (var i = 0; i < locations.length; i++) {
+        if (locations[i].name === theName) {
+          locations.splice(i, 1);
+          init();
+          return;
+        }
+      }
+    });
+  });
 });
